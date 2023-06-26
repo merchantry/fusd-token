98e5365eafc1ab4648eba3d219d4909508c32809
merchantry <fivanovic98@gmail.com>
2023-06-26 11:04:08 +0200

    updated boilerplate code

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,35 +1,22 @@
 const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
-const web3 = new Web3(
-  ganache.provider({
-    gasLimit: 1000000000000,
-  })
-);
 
 const contracts = require('../compile');
 
 const tokenContract = contracts['MyToken.sol'].MyToken;
 
-const {
-  getDeploy
-} = require('../utils/helper');
+const { deploy, getAccounts } = require('../utils/useWeb3');
 
 describe('MyToken tests', () => {
   let accounts, MyToken;
 
   beforeEach(async () => {
-    const deploy = getDeploy(web3);
-    accounts = await web3.eth.getAccounts();
+    accounts = await getAccounts();
     MyToken = await deploy(tokenContract, [], accounts[0]);
-
   });
-
 
   describe('MyToken', () => {
     it('deploys successfully', () => {
       assert.ok(MyToken.options.address);
     });
   });
-  
 });

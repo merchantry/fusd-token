bb78f33b2632dfaa943597d07cf0b4003e190534
merchantry <fivanovic98@gmail.com>
2023-06-26 16:25:04 +0200

    added openzeppelin contracts

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.19;
 
-contract MyToken {}
+import "./openzeppelin/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("StarDucks Cappucino Token", "CAPPU") {}
+}

9f11cb783919b13431dd911132289bfcbb2a8360
merchantry <fivanovic98@gmail.com>
2023-06-26 21:03:31 +0200

    refactored compiler

--- compile.js
+++ compile.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
-const paths = require('./contracts');
 const { formatCompileErrors } = require('./utils/debug');
 const { getAllFilesInFolder } = require('./utils/compile');
 
@@ -17,21 +16,14 @@ const input = {
   },
 };
 
-paths
-  .flatMap((file) => {
-    const pathToFile = path.resolve(__dirname, 'contracts', file);
-    const itemStat = fs.statSync(pathToFile);
-    if (itemStat.isFile()) return pathToFile;
-    return getAllFilesInFolder(pathToFile);
-  })
-  .forEach((file) => {
-    const fileName = file.split('contracts\\').pop().replace(/\\/g, '/');
-    const source = fs.readFileSync(file, 'utf8');
+getAllFilesInFolder(path.join(__dirname, 'contracts')).forEach((file) => {
+  const fileName = file.split('contracts\\').pop().replace(/\\/g, '/');
+  const source = fs.readFileSync(file, 'utf8');
 
-    input.sources[fileName] = {
-      content: source,
-    };
-  });
+  input.sources[fileName] = {
+    content: source,
+  };
+});
 
 const compiledInfo = JSON.parse(solc.compile(JSON.stringify(input)));
 if (compiledInfo.errors) console.error(formatCompileErrors(compiledInfo));

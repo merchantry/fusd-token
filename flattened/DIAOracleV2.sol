f4cd0bb11d1401540951be13a209670c6d8db13f
merchantry <fivanovic98@gmail.com>
2023-07-11 19:28:31 +0200

    added flattened contracts

---
+++ flattened/DIAOracleV2.sol
@@ -0,0 +1,42 @@
+// Sources flattened with hardhat v2.16.1 https://hardhat.org
+
+// File contracts/oracles/DIAOracleV2.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract DIAOracleV2 {
+    mapping(string => uint256) public values;
+    address oracleUpdater;
+
+    event OracleUpdate(string key, uint128 value, uint128 timestamp);
+    event UpdaterAddressChange(address newUpdater);
+
+    constructor() {
+        oracleUpdater = msg.sender;
+    }
+
+    function setValue(
+        string memory key,
+        uint128 value,
+        uint128 timestamp
+    ) public {
+        require(msg.sender == oracleUpdater);
+        uint256 cValue = (((uint256)(value)) << 128) + timestamp;
+        values[key] = cValue;
+        emit OracleUpdate(key, value, timestamp);
+    }
+
+    function getValue(string memory key) external view returns (uint128, uint128) {
+        uint256 cValue = values[key];
+        uint128 timestamp = (uint128)(cValue % 2**128);
+        uint128 value = (uint128)(cValue >> 128);
+        return (value, timestamp);
+    }
+
+    function updateOracleUpdaterAddress(address newOracleUpdaterAddress) public {
+        require(msg.sender == oracleUpdater);
+        oracleUpdater = newOracleUpdaterAddress;
+        emit UpdaterAddressChange(newOracleUpdaterAddress);
+    }
+}

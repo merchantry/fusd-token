d611d4ed177778cb9e964ca0f9ba1b7aa01de54d
merchantry <fivanovic98@gmail.com>
2023-10-01 14:39:30 +0200

    upgraded solidity version

--- contracts/FUSDTokenSaleUtils/TimeHandler.sol
+++ contracts/FUSDTokenSaleUtils/TimeHandler.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.21;
 
 abstract contract TimeHandler {
     function time() internal view returns (uint256) {
--- contracts/libraries/StringUtils.sol
+++ contracts/libraries/StringUtils.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.21;
 
 library StringUtils {
     function compare(string memory a, string memory b) internal pure returns (bool) {
